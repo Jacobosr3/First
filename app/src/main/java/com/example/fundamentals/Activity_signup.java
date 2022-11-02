8b933b794b9daab5dade213a9408905b2127324b
Jacobo Sánchez <jacosanro@gmail.com>
2022-11-02 12:13:27 +0100

    commit avtivity_signup

--- app/src/main/java/com/example/fundamentals/Activity_signup.java
+++ app/src/main/java/com/example/fundamentals/Activity_signup.java
@@ -3,12 +3,31 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Activity_signup extends AppCompatActivity {
+
+    EditText username, password;
+    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        cancel=findViewById(R.id.cancel);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username.setText("");
+                password.setText("");
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fundamentals/MainActivity.java
+++ app/src/main/java/com/example/fundamentals/MainActivity.java
@@ -2,27 +2,45 @@ package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button up;
+    Button up, cancel;
+    EditText username, password, email;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         up=findViewById(R.id.up);
+        cancel=findViewById(R.id.cancel);
+        username=findViewById(R.id.username);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
 
         up.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View view) {
                 paginaSiguiente();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username.setText("");
+                email.setText("");
+                password.setText("");
+
             }
         });
     }
