3fd279b81fc6d85195e850c377fadfe35b7864a7
Jacobo Sánchez <jacosanro@gmail.com>
2022-11-17 12:17:59 +0100

    commit cambio de imagen

--- app/src/main/java/com/example/fundamentals/Splash.java
+++ app/src/main/java/com/example/fundamentals/Splash.java
@@ -19,18 +19,17 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         openApp();
 
-        ImageView mSea = findViewById(R.id.prfondo);
+        ImageView mar = findViewById(R.id.prfondo);
 
         Glide.with(this)
-                .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                .load(R.drawable.marea)
 //                .load(R.drawable.girl)
                 .transition(DrawableTransitionOptions.withCrossFade(100))
                 .centerCrop()
 //                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
 //                .circleCrop()
-                .into(mSea);
-
+                .into(mar);
     }
 
     private void openApp() {
